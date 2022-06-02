1b55bf19d414a903694509e0c74a5b96859e9daa
PanwarAshish <panwar.ashish10@gmail.com>
2022-06-02 20:22:23 +0530

    chore: terraform backend basic

---
+++ 08-TerraformStates/01/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "initkloud_ec2_instance" {
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "t2.small"
+  tags = {
+    "Name" : "initkloud_ec2_state_example"
+  }
+  key_name          = "terraform-learning"
+  availability_zone = "us-east-1a"
+}
---
+++ 08-TerraformStates/02/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.1.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.1"
+    }
+  }
+  backend "s3" {
+    bucket = "initkloud-terraform-state-bucket"
+    key    = "development/terraform.tfstate"
+    region = "us-east-1"
+    profile = "terraform-initkloud-dev"
+    dynamodb_table = "initkloud-terraform-state-db"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-initkloud-dev"
+}
